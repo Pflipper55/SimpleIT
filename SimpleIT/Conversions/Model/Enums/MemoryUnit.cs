0b65beedd6268ac78f508f1bce721967ddbd1c97
Philipp Leon <phil897414@gmail.com>
2024-12-09 18:31:48 +0100

    Memory Conversion

---
+++ SimpleIT/Conversions/Model/Enums/MemoryUnit.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleIT.Conversions.Model.Enums;
+
+public enum MemoryUnit : long
+{
+    BITS = 1,
+    BYTE = 8,
+    KILOBYTE = 1000,
+    MEGABYTE = 1000000,
+    GIGABYTE = 1000000000,
+    TERRABYTE = 1000000000000,
+    KIBIBYTE = 1024,
+    MIBIBYTE = 1048576,
+    GIBIBYTE = 1073741824,
+    TIBIBYTE = 1099511627776,
+}
